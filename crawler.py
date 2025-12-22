a011e7a7017ee966dffd12554a7e2438555c92c0
vedori <88069247+vedori@users.noreply.github.com>
2025-12-22 00:46:11 -0500

    pywikibot infobox parse wip

--- crawler.py
+++ crawler.py
@@ -4,17 +4,12 @@
 import pywikibot
 from pywikibot import textlib
 
-headers = {
-    "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Accept-Language": "en",
-}
-
 site = pywikibot.Site("wikipedia:en")
 
 
 def get_state_info(state: str):
     state_page = pywikibot.Page(site, state)
     sect = textlib.extract_sections(state_page.text, site)
+    # test_parser = textlib.GetDataHTML(keeptags = ['infobox'])
 
     pass
